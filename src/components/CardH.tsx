09453be3a277aa96f9c232a5faa26ae2dc5fa6e1
Jay Odedra <jayodedra9919@gmail.com>
2025-01-25 23:24:32 +0530

    update price-tracker ui

---
+++ src/components/CardH.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function CardH({ coin }: any) {
+    return (
+        <div>
+            <Link href={"/"}>
+            <div className="flex items-center gap-2 justify-between bg-theme_gray-light dark:bg-theme_gray-dark p-4 rounded-lg">
+            <div className="flex gap-2 items-center">
+                <img
+                    src={coin.image}
+                    alt={coin.name}
+                    width={30}
+                    height={30}
+                    className="mx-auto mb-4"
+                />
+                <h2 className="text-xl font-semibold font-semibold mb-4">
+                    {coin.name}
+                </h2>
+            </div>
+            <p className="text-lg">
+                <strong>Current Price:</strong> ${coin.current_price}
+            </p>
+            <p className="text-lg">
+                <strong>24H High:</strong> ${coin.high_24h}
+            </p>
+            <p className="text-lg mb-4">
+                <strong>24H Low:</strong> ${coin.low_24h}
+            </p>
+            {/* Render the Graph for each coin */}
+            {/* <Graph coinId={coin.id} /> */}
+        </div>
+            </Link>
+        </div>
+    )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link"
+
+export default function Sidebar({togglemode}: {togglemode: () => void}) {
+    return (
+        <div className="bg-theme_black-light dark:bg-theme_black-dark text-black dark:text-white h-screen rounded-3xl p-4 text-center">
+            <div className="bg-clip-text bg-theme_red-light dark:bg-theme_red-dark">
+                <h1 className="text-transparent text-4xl font-semibold">PLUTUS</h1>
+            </div>
+            <hr className="m-5" />
+            <div>
+                <ul className="flex flex-col gap-3">
+                    <li>
+
+                        <Link href={"/"}><i className="fa-solid fa-house"></i>Home</Link>
+                    </li>
+                    <li>
+                        {/* <Link href={"/price-tracker"}><i className="fa-regular fa-chart-bar"></i>Prices</Link> */}
+                    </li>
+                </ul>
+            </div>
+            <div className="m-5 cursor-pointer" onClick={togglemode}>
+                Darkmode
+            </div>
+        </div>
+    )
+}
